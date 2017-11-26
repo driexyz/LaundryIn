2b325ef1acfb86845f15d85a8b5eba5e71bc4c1e
driexyz <driexyz@live.com>
2017-11-26 23:18:59 +0700

    Initial commit
    *still crash at first launch

---
+++ app/src/main/java/com/drpro/laundryin/MainActivity.java
@@ -0,0 +1,37 @@
+package com.drpro.laundryin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(signIn);
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
