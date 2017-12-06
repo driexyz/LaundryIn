e4ef2cd9996e1757ebdd270b5f1bdec6b2ef46b6
driexyz <driexyz@live.com>
2017-12-06 22:55:38 +0700

    * add dummy tab to history fragment(wip) * add image banner to home

--- app/src/main/java/com/drpro/laundryin/BlankFragment.java
+++ app/src/main/java/com/drpro/laundryin/BlankFragment.java
@@ -3,15 +3,26 @@ package com.drpro.laundryin;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hold1.pagertabsindicator.PagerTabsIndicator;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BlankFragment extends Fragment {
+
+    private ViewPager viewPager;
+    private PagerTabsIndicator tabsIndicator;
+    private PagerAdapter viewTextAdapter;
+    private Fragment demoFragments;
 
 
     public BlankFragment() {
@@ -23,7 +34,33 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        viewPager = view.findViewById(R.id.view_pager);
+        tabsIndicator = view.findViewById(R.id.tabs_indicator);
+
+        viewTextAdapter = new TextAdapter(getActivity().getSupportFragmentManager());
+
+        viewPager.setAdapter(viewTextAdapter);
+        tabsIndicator.setViewPager(viewPager);
+
+        return view;
     }
 
+    class TextAdapter extends FragmentPagerAdapter {
+
+        public TextAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return demoFragments;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 }
--- app/src/main/java/com/drpro/laundryin/HomeFragment.java
+++ app/src/main/java/com/drpro/laundryin/HomeFragment.java
@@ -1,9 +1,9 @@
 package com.drpro.laundryin;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +12,30 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import ss.com.bannerslider.banners.Banner;
+import ss.com.bannerslider.banners.RemoteBanner;
+import ss.com.bannerslider.views.BannerSlider;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener {
 
 
     private TextView
@@ -27,7 +43,12 @@ public class HomeFragment extends Fragment {
             mTextCurDate,
             mTextETADate;
 
+    private BannerSlider bannerSlider;
+
     public static final int REQUEST_CODE = 1;
+
+    private GoogleApiClient mGoogleApiClient;
+    private int PLACE_PICKER_REQUEST = 1;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -45,8 +66,15 @@ public class HomeFragment extends Fragment {
         mTextLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openMaps = new Intent(getActivity(),MapsActivity.class);
-                startActivityForResult(openMaps, REQUEST_CODE);
+                //Intent openMaps = new Intent(getActivity(),MapsActivity.class);
+                //startActivityForResult(openMaps, REQUEST_CODE);
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -61,6 +89,16 @@ public class HomeFragment extends Fragment {
 
         mTextCurDate.setText(getCurrentDate());
         mTextETADate.setText(getEstimateDate());
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(getActivity())
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(getActivity(), this)
+                .build();
+
+        bannerSlider = (BannerSlider) view.findViewById(R.id.banner_slider1);
+        addBanners();
 
         return view;
     }
@@ -88,17 +126,87 @@ public class HomeFragment extends Fragment {
         return formattedDate;
     }
 
-    @Override
+    private void addBanners(){
+        List<Banner> remoteBanners=new ArrayList<>();
+        //Add banners using image urls
+        remoteBanners.add(new RemoteBanner(
+                "https://assets.materialup.com/uploads/dcc07ea4-845a-463b-b5f0-4696574da5ed/preview.jpg"
+        ));
+        remoteBanners.add(new RemoteBanner(
+                "https://assets.materialup.com/uploads/4b88d2c1-9f95-4c51-867b-bf977b0caa8c/preview.gif"
+        ));
+        remoteBanners.add(new RemoteBanner(
+                "https://assets.materialup.com/uploads/76d63bbc-54a1-450a-a462-d90056be881b/preview.png"
+        ));
+        remoteBanners.add(new RemoteBanner(
+                "https://assets.materialup.com/uploads/05e9b7d9-ade2-4aed-9cb4-9e24e5a3530d/preview.jpg"
+        ));
+        bannerSlider.setBanners(remoteBanners);
+
+    }
+
+
+    /*@Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (requestCode == REQUEST_CODE) {
 
-            if (resultCode == Activity.RESULT_OK) {
+            if (resultCode == RESULT_OK) {
                 mTextLocation.setText(data.getStringExtra("pos"));
                 // do something with the result
 
             } else if (resultCode == Activity.RESULT_CANCELED) {
                 // some stuff that will happen if there's no result
+            }
+        }
+    }*/
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.stopAutoManage(getActivity());
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getContext());
+                StringBuilder stBuilder = new StringBuilder();
+                String placename = String.format("%s", place.getName());
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                String address = String.format("%s", place.getAddress());
+                stBuilder.append("Name: ");
+                stBuilder.append(placename);
+                stBuilder.append("\n");
+                stBuilder.append("Latitude: ");
+                stBuilder.append(latitude);
+                stBuilder.append("\n");
+                stBuilder.append("Logitude: ");
+                stBuilder.append(longitude);
+                stBuilder.append("\n");
+                stBuilder.append("Address: ");
+                stBuilder.append(address);
+                mTextLocation.setText(stBuilder.toString());
             }
         }
     }
